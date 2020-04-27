3e3c11e7ced5603229c1f01003b57043a022601c
Glitch (hello-express) <none>
2020-04-27 03:00:40 +0000

    🏑🍉 Checkpoint ./controllers/user.controller.js:15276811/1138

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -4,7 +4,8 @@ const bcrypt = require('bcrypt');
 const saltRounds = 10;
 
 module.exports.index = function(req, res) {
-        const sgMail = require('@sendgrid/mail');
+  console.log(1);
+      const sgMail = require('@sendgrid/mail');
       sgMail.setApiKey(process.env.SENDGRID_API_KEY);
       const msg = {
         to: 'test@example.com',
@@ -13,7 +14,14 @@ module.exports.index = function(req, res) {
         text: 'and easy to do anywhere, even with Node.js',
         html: '<strong>and easy to do anywhere, even with Node.js</strong>',
       };
-      sgMail.send(msg);
+      sgMail.send(msg)
+      .then(() => {}, error => {
+        console.error(error);
+
+        if (error.response) {
+          console.error(error.response.body)
+        }
+      });
   res.render('user/index');
 };
 
