0b3e7734e5b6efa50a67179caf48591353ab6714
Glitch (hello-express) <none>
2020-04-28 08:11:14 +0000

    🚡🔮 Checkpoint ./controllers/blog.controller.js:15276811/29 ./views/blog/index.pug:15276811/641

--- controllers/blog.controller.js
+++ controllers/blog.controller.js
@@ -1,8 +1,8 @@
 var db = require('../db');
 
 module.exports.index = function(req, res) {
-  var q = req.query;
-  var blogs = db.get('blogs').value().slice((q-1)*8, q*8);
+  var page = req.query.page || 1;
+  var blogs = db.get('blogs').value().slice((page-1)*8, page*8);
   res.render('blog/index', {
     blogs: blogs
   });
