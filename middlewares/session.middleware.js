75cc89a1d0b6d11c5913de8f991a12a8fae52067
Glitch (hello-express) <none>
2020-04-29 06:37:32 +0000

    🌍🐚 Checkpoint ./middlewares/session.middleware.js:15276811/454 ./db.json:15276811/78

--- middlewares/session.middleware.js
+++ middlewares/session.middleware.js
@@ -1,11 +1,16 @@
 var shortid = require('shortid');
+var db = require('../db');
 
 module.exports = function(req, res, next) {
   if(!req.signedCookies.sessionId) {
-    res.cookie('sessionId', shortid.generate(), {
+    var sessionId = shortid.generate();
+    res.cookie('sessionId', sessionId, {
         signed: true
       });
+    db.get('sessions').push({id: sessionId}).write();
+    console.log(db.get('sessions').value());
   }
+  
   
   next();
 }
